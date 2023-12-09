cf00f5f387a35e55f480f7d176add0a472acaab8
Ameerhwais <144728369+Ameerhwais@users.noreply.github.com>
2023-12-09 02:56:47 +0000

    assigment 5

--- script.js
+++ script.js
@@ -1,78 +1,121 @@
+//
+//  JS File
+//  YOU CAN REMOVE ALL OF THIS CODE AND START FRESH
+//
+
+//
 // Variables
-const appID = 'app';
-const projectDueDate = '2023-12-31';
-const headingText = 'Project Deadline:';
-const headingTextIcon = ' 🚀';
+//
 
 // Constants
-const countdownDate = new Date(projectDueDate);
+const appID = "app";
+const headingText = "TASK HERO.";
+const headingTextIcon = "🚀";
+const projectDueDate = "8 December 2023 11:59";
+
+// Variables
+let countdownDate = new Date(projectDueDate);
 
 // DOM Elements
-const appContainer = document.getElementById(appID);
+let appContainer = document.getElementById(appID);
 
+//
 // Functions
+//
 
 function calculateDaysLeft(countdownDate) {
   const now = new Date().getTime();
   const countdown = new Date(countdownDate).getTime();
 
+  console.log(countdown);
+
   const difference = (countdown - now) / 1000;
 
+
+  // Countdown passed already
   if (difference < 1) {
     return null;
   }
 
+
   const days = Math.floor(difference / (60 * 60 * 24));
+
   return days;
 }
 
-function initializeTaskList() {
-  const taskListSection = document.getElementById('task-list');
-  const addTaskForm = document.getElementById('add-task-form');
-
-  if (!taskListSection || !addTaskForm) {
-    console.error("Error: Could not find task list or add task form");
+// Add a heading to the app container
+function inititialise() {
+  // If anything is wrong with the app container then end
+  if (!appContainer) {
+    console.error("Error: Could not find app contianer");
     return;
   }
 
-  addTaskForm.addEventListener('submit', function (event) {
-    event.preventDefault();
+  // Create an h1 and add it to our app
+  const h1 = document.createElement("h1");
+  const daysLeft = calculateDaysLeft(countdownDate);
+  let headingTextCalculated = headingText;
 
-    const taskNameInput = document.getElementById('task-name');
-    const taskDateInput = document.getElementById('task-date');
-    const taskName = taskNameInput.value.trim();
-    const taskDate = taskDateInput.value;
+  if (daysLeft > 1) {
+    headingTextCalculated = headingTextCalculated.concat(
+      " In ",
+      daysLeft.toString(),
+      " days "
+    );
+  }else if (daysLeft === 1) {
+    headingTextCalculated = headingTextCalculated.concat(
+      " Tomorrow"
+    );
+  }
 
-    if (taskName !== '' && taskDate !== '') {
-      addTask(taskName, taskDate);
-      taskNameInput.value = '';
-      taskDateInput.value = '';
-    }
+  h1.textContent = headingTextCalculated.concat(headingTextIcon);
+  appContainer.appendChild(h1);
+
+  // Init complete
+  console.log("App successfully initialised");
+}
+
+//
+// Inits & Event Listeners
+//
+
+inititialise();
+document.addEventListener('DOMContentLoaded', function() {
+  const taskListSection = document.getElementById('task-list');
+  const addTaskForm = document.getElementById('add-task-form');
+  const calendarSection = document.getElementById('calendar');
+
+  addTaskForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      
+      const taskNameInput = document.getElementById('task-name');
+      const taskDateInput = document.getElementById('task-date');
+      const taskName = taskNameInput.value.trim();
+      const taskDate = taskDateInput.value;
+
+      if (taskName !== '' && taskDate !== '') {
+          addTask(taskName, taskDate);
+          taskNameInput.value = '';
+          taskDateInput.value = '';
+      }
   });
 
   function addTask(taskName, taskDate) {
-    const taskItem = document.createElement('div');
-    taskItem.classList.add('task-item');
-    taskItem.innerHTML = `<p>${taskName}</p><p>Date: ${taskDate}</p>`;
-    taskListSection.appendChild(taskItem);
+      const taskItem = document.createElement('div');
+      taskItem.classList.add('task-item');
+      taskItem.innerHTML = `<p>${taskName}</p><p>Date: ${taskDate}</p>`;
+      taskListSection.appendChild(taskItem);
   }
-}
 
-// Inits & Event Listeners
-
-document.addEventListener('DOMContentLoaded', function () {
-  initializeTaskList();
-
-  const calendarSection = document.getElementById('calendar');
-
+  // Initialize FullCalendar
   $('#calendar').fullCalendar({
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month,basicWeek,basicDay'
-    },
-    defaultDate: moment(),
-    editable: true,
-    events: [] 
+      header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,basicWeek,basicDay'
+      },
+      defaultDate: moment(),
+      editable: true,
+      events: [] // You can populate events dynamically
   });
 });
